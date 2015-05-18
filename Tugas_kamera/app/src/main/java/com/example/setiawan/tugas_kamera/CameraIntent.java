183152c9f6ddd5781fc80b462d9ec47536aa0b15
setiawanprakoso <setiawanxprakoso@gmail.com>
2015-05-18 23:39:27 +0700

    no message

---
+++ Tugas_kamera/app/src/main/java/com/example/setiawan/tugas_kamera/CameraIntent.java
@@ -0,0 +1,29 @@
+package com.example.setiawan.tugas_kamera;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class CameraIntent extends Activity {
+    final static int CAMERA_RESULT = 0;
+    ImageView imv;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_intent);
+        Intent i = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(i, CAMERA_RESULT);
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (resultCode == RESULT_OK)
+        {
+            Bundle extras = intent.getExtras();
+            Bitmap bmp = (Bitmap) extras.get("data");
+            imv = (ImageView) findViewById(R.id.ImageReturnedView);
+            imv.setImageBitmap(bmp);
+        }
+    }
+}
